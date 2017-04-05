d8c34cac21c4554f2fd480c022c56edf9fd395dc
Rick Barrette <rickbarrette@gmail.com>
2017-04-04 22:54:16 -0400

    Create 001_create_bestest_timers.rb

---
+++ db/migrate/001_create_bestest_timers.rb
@@ -0,0 +1,15 @@
+class CreateBestestTimers < ActiveRecord::Migration
+  def self.up
+    create_table :bestest_timers do |t|
+      t.column :user_id, :integer
+      t.column :issue_id, :integer
+      t.column :started_on, :datetime
+      t.column :time_spent, :float, :default => 0
+      t.column :paused, :boolean, :default => false
+    end
+  end
+
+  def self.down
+    drop_table :time_loggers
+  end
+end
