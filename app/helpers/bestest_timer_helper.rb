1d4515f27f99d30f3c4a3feb364a726bbb16b1aa
Rick Barrette <rickbarrette@gmail.com>
2017-04-04 23:09:21 -0400

    Create bestest_timer_helper.rb

---
+++ app/helpers/bestest_timer_helper.rb
@@ -0,0 +1,9 @@
+module BestestTimerHelper
+    def issue_from_id(issue_id)
+        Issue.find_by_id(issue_id)
+    end
+
+    def user_from_id(user_id)
+        User.find_by_id(user_id)
+    end
+end
